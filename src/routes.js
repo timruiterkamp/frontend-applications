0aeab3b47552209dd2a70813fbc94746f27bdeec
timruiterkamp <timruiterkamp@gmail.com>
2018-10-10 13:17:36 +0200

    added scss and routes

---
+++ src/routes.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
+import App from './containers/App';
+export default () => {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route exact path='/' component={App}/>
+            </Switch>
+        </BrowserRouter>
+    )
+}
