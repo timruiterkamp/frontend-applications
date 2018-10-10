0e090b9caa5d1297acbf1fc65245dc6070176e15
timruiterkamp <timruiterkamp@gmail.com>
2018-10-10 13:32:57 +0200

    setup basic routing

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
-import App from './containers/App';
+import App from './pages/App';
+
 export default () => {
     return (
         <BrowserRouter>
