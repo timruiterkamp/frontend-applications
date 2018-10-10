25579ecaea114c3341e6c7b410d5730b4cce05ed
timruiterkamp <timruiterkamp@gmail.com>
2018-10-10 16:26:27 +0200

    build layout component that combines header and footer with content

---
+++ src/components/page/Header.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const Header = props => (
+    <header className="App-header">
+        {props.children}
+    </header>
+);
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import App from './pages/App';
 
 export default () => {
