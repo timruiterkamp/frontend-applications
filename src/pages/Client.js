ed7f5493b3ba9f48834daa608209ae136236ebd8
timruiterkamp <timruiterkamp@gmail.com>
2018-10-11 15:04:45 +0200

    setup basic 404 page

--- src/pages/Client.js
+++ src/pages/Client.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
-export const Client = ({ match }) => (
+export const Client = ({ match }, props) => (
     <div>
         <h2> title</h2>
         <h3>ID: {match.params.id}</h3>
+        <h3>Name: {props.name}</h3>
     </div>
 );
--- src/routes.js
+++ src/routes.js
@@ -3,15 +3,17 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import App from './pages/App';
 import Clients from './pages/Clients';
 import {Client} from './pages/Client';
+import {NotFound} from './pages/404';
 
 
-export default () => {
+export default () => {  
     return (
         <BrowserRouter>
             <Switch>
                 <Route exact path='/' component={App}/>
                 <Route path='/clienten/:id' component={Client}/>
                 <Route path='/clienten' component={Clients}/>
+                <Route path='*' component={NotFound} />
             </Switch>
         </BrowserRouter>
     )
